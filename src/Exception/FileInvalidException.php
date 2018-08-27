6567dd4ab2050b6a9c0130c2ead659ec9fd692d3
Nguyen Nhu Tuan <tuanquynh0508@gmail.com>
2018-08-27 16:20:43 +0700

    Rename Class Upload, Add Namesapce, add Exception Handler

---
+++ src/Exception/FileInvalidException.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @author Nguyen Nhu Tuan <tnguyennhu@pentalog.com>
+ */
+
+namespace Verot\Uploader\Exception;
+
+class FileInvalidException extends \InvalidArgumentException
+{
+
+}
